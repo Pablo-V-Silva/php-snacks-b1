e5584e3c35dfb62c5c5bfd862b718332f5417425
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-12-02 15:50:40 +0100

    exercize complete

---
+++ snack3/index.php
@@ -0,0 +1,69 @@
+<?php
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <div>
+    <?php
+    foreach ($posts as $key => $post) { ?>
+
+      <p>-<?= $key; ?></p>
+
+      <?php for ($i=0; $i < count($post); $i++) { ?>
+        <p><?= $post[$i]['title']; ?></p>
+        <ul>
+          <li><?= $post[$i]['author']; ?></li>
+          <li><?= $post[$i]['text']; ?></li>
+        </ul>
+      <?php }
+
+     } ?>
+  </div>
+</body>
+</html>
